502ea728d3328695a0bb814882464474fca3640f
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-10-03 21:15:31 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,92 @@
+        
+    // var pass = "";
+
+    // var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
+
+    // var passLength = +prompt("Enter password length:");
+    
+    // for ( var i = 1 ; i <= passLength ; i++ ) {
+    //     var randomNum = Math.floor(Math.random() * str.length + 1 );
+    //     pass += str[randomNum];
+    // }
+
+    // console.log(pass);
+
+
+
+// Chap 31 Getting the current date and time:
+
+
+var date = new Date();
+
+// console.log(date);
+
+// console.log(typeof date);
+
+// console.log(date.slice(0,10));
+
+// console.log(date.toString().slice(0,15));
+
+
+
+var date = new Date();
+
+// var daysArr = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+// ];
+
+// console.log(daysArr[date.getDay()]);
+
+
+// var date = new Date();
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+
+
+// var date = new Date();
+
+// console.log(date.getTime());
+
+
+// AGE CALCULATOR PROGRAM:
+
+// var userBirthYear = +prompt("Enter your birth year");
+// var currentYear = new Date();
+// var userAge = currentYear.getFullYear() - userBirthYear
+
+// console.log(`Your age is ${userAge}`);
+
+
+// var date = new Date("25 Feb 2014 15:35:40");
+
+// console.log(date);
+
+
+
+// var firstRamadan = new Date("March 1, 2025").getTime();
+
+// var current = new Date().getTime();
+
+// var diff = firstRamadan - current;
+
+// var month = diff / (1000 * 60 * 60 * 24 * 30);
+
+// console.log(Math.round(month));
+
+// console.log(`First Ramadan:  ${firstRamadan}`);
+
+// console.log(`Current:  ${current}`);
+
+// console.log(`Left: ${timeLeft}`);
+
+
+
+// ---------------------------------------------------------------------------------
